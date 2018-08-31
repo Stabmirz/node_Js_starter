af93b2d1303110dcfdd6573e8e48ad3ea5f6b716
Stabmirz <amirza@techlaunch.io>
2018-08-31 00:46:57 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+let postsJSON = require('../posts.json')
+
+
+exports.home =(req, res)=>{
+    const posts = postsJSON.posts;
+    res.render('home', {
+    title: 'Posts',
+    posts: posts
+    })
+}
+
+exports.test =(req, res)=>{
+    res.render('test');
+}
